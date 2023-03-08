9cb15df86c7bb748203c47dfafbf3884681a9194
sanket-brucira <sanket.k@brucira.com>
2023-03-08 18:09:12 +0530

    wip: cart add,delete,update

--- src/main/java/com/ecom/proto/controller/CartController.java
+++ src/main/java/com/ecom/proto/controller/CartController.java
@@ -3,7 +3,9 @@ package com.ecom.proto.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,10 @@ public class CartController{
     @PostMapping
     public ResponseEntity<?> addToCartItems(@RequestHeader("token") String token, @RequestParam(name = "id") long itemId){
         return service.addToCartItems(token, itemId);
+    }
+    @DeleteMapping("{productID}")
+    public ResponseEntity<?> deleteCartItems(@RequestHeader("token") String token, @PathVariable String productID) {
+        return service.deleteCartItems(token, productID);
     }
 
 }
