42a29262c536cf38e78cd3c9ac3bfbe70c567699
shubham.s <shubham.s@brucira.com>
2023-03-14 17:33:27 +0530

    added crossorigin

--- src/main/java/com/ecom/proto/controller/AuthController.java
+++ src/main/java/com/ecom/proto/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.ecom.proto.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.ecom.proto.model.AuthDTO;
 import com.ecom.proto.service.AuthService;
 
+@CrossOrigin
 @Controller
 public class AuthController{
 
