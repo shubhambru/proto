4acde501e5ed823f7c8781b16e340269a0cfa6c8
sanket-brucira <sanket.k@brucira.com>
2023-03-15 14:27:59 +0530

    update: sort by price

--- src/main/java/com/ecom/proto/controller/ProductController.java
+++ src/main/java/com/ecom/proto/controller/ProductController.java
@@ -5,7 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecom.proto.service.ProductService;
 
@@ -21,4 +23,11 @@ public class ProductController {
     public ResponseEntity<?> getAllProducts(){
         return service.getAllProducts();
     }
+
+    @GetMapping("/{order}")
+    public ResponseEntity<?> getAllSortProducts(@PathVariable String order ){
+        return service.getAllSortProducts(order);
+    }
+    
 }
+
